a092152095b9e2543382d69d5ef7ddcafd0db693
hanajihanaji <hanajihanaji@gmail.com>
2025-08-15 16:20:21 +0900

    fix: Improve database connection configuration for Railway

--- cafepon-cms/config/database.ts
+++ cafepon-cms/config/database.ts
@@ -10,10 +10,13 @@ export default ({ env }) => {
         client: 'postgres',
         connection: {
           connectionString: env('DATABASE_URL'),
-          ssl: env.bool('DATABASE_SSL', false) && {
-            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-          },
+          ssl: false,
         },
+        pool: { 
+          min: env.int('DATABASE_POOL_MIN', 2), 
+          max: env.int('DATABASE_POOL_MAX', 10) 
+        },
+        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
         debug: false,
       },
     };
